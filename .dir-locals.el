b05c3ee3b67d5f86a84ce34f191c176a5228c6b6
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2019-03-08 22:09:09 +0300

    Improve styles; remove author's name
    * Use material design from https://getmdl.io/
    * Improve styles.
    * Remove author's name from the site.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,8 +3,7 @@
       . (("ru"
           "\
 <table style=\"width: 100%%\"><tr><td>
-<p class=\"author\">Author: %a</p>\n
 <p class=\"date\">Date: %d</p>\n<p class=\"creator\">%c</p>\n<p class=\"validation\">%v</p>
-</td><td style=\"lambda\">
+</td><td>
 <center><div class=\"lambda\"><a href=\"https://lispers.org/\">λ</a></div></center>
 </td></table>"))))))
