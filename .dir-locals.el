f8bb1aa2a9687762b5c73727450a4906d90c15e7
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2019-03-08 20:38:18 +0300

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+((nil
+  . ((org-html-postamble-format
+      . (("ru"
+          "\
+<table style=\"width: 100%%\"><tr><td>
+<p class=\"author\">Author: %a</p>\n
+<p class=\"date\">Date: %d</p>\n<p class=\"creator\">%c</p>\n<p class=\"validation\">%v</p>
+</td><td style=\"lambda\">
+<center><div class=\"lambda\"><a href=\"https://lispers.org/\">λ</a></div></center>
+</td></table>"))))))
