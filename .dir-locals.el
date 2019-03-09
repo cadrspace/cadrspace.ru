15bc10be0269cbadf9c6a26c2113998a6ccd0a04
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2019-03-09 08:48:45 +0300

    Update the footer; improve the overall design

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,12 @@
       . (("ru"
           "\
 <table style=\"width: 100%%\"><tr><td>
-<p class=\"date\">Date: %d</p>\n<p class=\"creator\">%c</p>\n<p class=\"validation\">%v</p>
-</td><td>
-<center><div class=\"lambda\"><a href=\"https://lispers.org/\">λ</a></div></center>
-</td></table>"))))))
+<p class=\"date\">Updated: %d</p>\n<p class=\"creator\">%c</p>\n
+<p class=\"validation\">
+Made with <a href=\"https://lispers.org/\" class=\"lambda\">λ</a>
+(<a href=\"https://github.com/cadrspace/cadrspace.ru\">source</a>) — %v</p>
+</td>
+<td style=\"min-width: 50%%\">
+<img src=\"images/invader.png\" align=\"middle\">
+<img src=\"images/invader.png\" align=\"middle\">
+</td></tr></table>"))))))
